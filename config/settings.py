19d43853d43d91163a93d720d2e875144d494815
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-09-24 17:12:04 +0900

    fix: setting 파일 수정

--- config/settings.py
+++ config/settings.py
@@ -155,7 +155,8 @@ CORS_ORIGIN_WHITELIST = [
                         'https://nuseum-949i9v22k-parkjju.vercel.app',
                         'http://127.0.0.1:3000',
                         'http://localhost:3000',
-                        'https://dev.example.com:3000']
+                        'https://dev.example.com:3000',
+                        'https://nuseum-fnqo.vercel.app']
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "config.urls"
