ce64fddd27110568be8fd4536e5f48b04046bb62
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-10-22 19:09:45 +0900

    fix : 내 아이 임시 api 수정

--- results/views.py
+++ results/views.py
@@ -5,7 +5,19 @@ from rest_framework.response import Response
 class TempExaminationResultView(APIView):
   def get(self, request):
     user = request.user.username
-    data = {
-      'user' : user
-    }
+    if user == 'NPP01':
+      data = {
+        'user' : user,
+        'data' : None
+      }
+    elif user == 'NPP02' or user == '오이':
+      data = {
+        'user' : user,
+        'data' : "https://s3.ap-northeast-2.amazonaws.com/jinhyung.test.aws/result/NPP02/2022.10.20+%EC%9D%BC%EB%B0%98%EA%B2%B0%EA%B3%BC%EC%A7%80+(NPP-02+%E3%84%B1%E3%85%8E%E3%84%B9).pdf"
+      }
+    else :
+      data = {
+        'user' : user,
+        'data' : None
+      }
     return Response(data=data)
