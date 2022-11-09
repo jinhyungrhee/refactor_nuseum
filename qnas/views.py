3db87c6a5fc22904b9f9deef4c3054324b1c97d1
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-11-09 15:31:59 +0900

    fix : qna pagination 임시 수정

--- qnas/views.py
+++ qnas/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 class CustomPagination(PageNumberPagination):
-  page_size = 20
+  page_size = 100
 
 # Question List & Create
 class QuestionAPIView(APIView):
