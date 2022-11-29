e1065c2f194dea68af93e0d7259625847fdba897
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-11-30 08:36:01 +0900

    feat: search supplement 로직 추가

--- foods/serializers.py
+++ foods/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Food
+from .models import Food, Supplement
 
 class FoodSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = Food
+    fields = '__all__'
+
+class SupplementSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = Supplement
     fields = '__all__'
--- foods/views.py
+++ foods/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Food
-from .serializers import FoodSerializer
+from .models import Food, Supplement
+from .serializers import FoodSerializer, SupplementSerializer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
@@ -14,11 +14,20 @@ class FoodsView(APIView):
     paginator =PageNumberPagination()
     paginator.page_size = 10
     search_query = request.GET.get("search", None)
-    # print(search_query)
-    if search_query != None:
-      foods = Food.objects.filter(Q(name__icontains=search_query)).order_by('classifier', 'id')
-    else: # 쿼리가 없으면 전체 음식 출력
-      foods = Food.objects.all()
-    results = paginator.paginate_queryset(foods, request)
-    serializer = FoodSerializer(results, many=True)
-    return paginator.get_paginated_response(data=serializer.data)
+    search_type = request.GET.get("type", None)
+    if search_type == "supplement": # supplement type (서로 다른 DB로 접근)
+      if search_query != None:
+        supplements = Supplement.objects.filter(Q(name__icontains=search_query)).order_by('id')
+      else:
+        supplements = Supplement.objects.all()
+      results = paginator.paginate_queryset(supplements, request)
+      serializer = SupplementSerializer(results, many=True)
+      return paginator.get_paginated_response(data=serializer.data)
+    else: # food type (서로 다른 DB로 접근)
+      if search_query != None:
+        foods = Food.objects.filter(Q(name__icontains=search_query)).order_by('classifier', 'id')
+      else: # 쿼리가 없으면 전체 음식 출력
+        foods = Food.objects.all()
+      results = paginator.paginate_queryset(foods, request)
+      serializer = FoodSerializer(results, many=True)
+      return paginator.get_paginated_response(data=serializer.data)
